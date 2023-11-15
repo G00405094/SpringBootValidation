d46db381c27e0e35d10075ef9ccd3d90f9d05ec1
G00405094 <G00405094@atu.ie>
2023-11-15 15:16:30 +0000

    Finished PersonController Class

--- src/main/java/ie/atu/springbootvalidation/PersonController.java
+++ src/main/java/ie/atu/springbootvalidation/PersonController.java
@@ -1,10 +1,10 @@
 package ie.atu.springbootvalidation;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/person")
 @RestController
@@ -12,6 +12,7 @@ public class PersonController {
     private final PersonService personService;
 
     public PersonController(PersonService personService){this.personService = personService;}
+
 
     @GetMapping("/{employeeId}")
     public ResponseEntity<?> getPerson(@PathVariable String employeeId){
@@ -27,4 +28,8 @@ public class PersonController {
 
         return ResponseEntity.ok(person);
     }
+    @PostMapping("/createPerson")
+    public ResponseEntity<String>create(@Valid @RequestBody Person person){
+        personService.savePerson(person);
+        return new ResponseEntity<>("Person created successfully", HttpStatus.OK);    }
 }
